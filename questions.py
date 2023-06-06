ac6ca11eb6a8dedcaffe37d807bc69184b642b96
corbynshailer <124210735+corbynshailer@users.noreply.github.com>
2023-06-06 20:23:47 +1200

    Add files via upload

---
+++ questions.py
@@ -0,0 +1,40 @@
+{
+    "question": "What has to be broken before you can use it?",
+    "correct_answer": "Egg"
+},
+{
+    "question": "What is New Zealands national bird?",
+    "correct_answer": "Kiwi"
+},
+{
+    "question": "Which planet is known as the Red Planet?",
+    "correct_answer": "Mars"
+},
+{
+    "question": "How many elements are in the periodic table?",
+    "correct_answer": "118"
+},
+{
+    "question": "Which country is known as the 'Land of the Rising Sun'?",
+    "correct_answer": "Japan"
+},
+{
+    "question": "Which is the only body part that is fully grown from birth?",
+    "correct_answer": "Eyes"
+},
+{
+    "question": "Which animal is known as the 'King of the Jungle'?",
+    "correct_answer": "Lion"
+},
+{
+    "question": "What is the largest organ in the human body?",
+    "correct_answer": "Skin"
+},
+{
+    "question": "Where did sushi originate?",
+    "correct_answer": "Chine"
+},
+{
+    "question": "What is the largest ocean on Earth?",
+    "correct_answer": "Pacific"
+},
